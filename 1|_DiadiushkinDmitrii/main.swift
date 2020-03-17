48fa508cf614c2d8db45b4a736180878bdb64ca4
Dmitrii Diadiushkin <stephandvd@yandex.ru>
2020-03-18 00:04:36 +0500

    First task complete

--- 1|_DiadiushkinDmitrii/main.swift
+++ 1|_DiadiushkinDmitrii/main.swift
@@ -8,5 +8,41 @@
 
 import Foundation
 
-print("Hello, World!")
+func Enter_Int(coef:String) -> Double {
+    var Coef_Int_Str:String
+    var Coef_Int:Double?
+    print("Введите значение переменной \(coef): ")
+    while(true){
+        Coef_Int_Str = readLine()!
+        Coef_Int = Double(Coef_Int_Str) ?? nil
+        if Coef_Int != nil{
+            break
+        }else{
+            print("Пожалуйста, введите число:")
+        }
+    }
+    return Coef_Int!
+}
 
+func equation() {
+    print("Добро пожаловать в программу, решающую квадратное уравнение вида ax2+bx+c=0")
+    let A = Enter_Int(coef: "A")
+    let B = Enter_Int(coef: "B")
+    let C = Enter_Int(coef: "C")
+    
+    let D:Double = B*B - 4*A*C
+    
+    if D > 0 {
+        let x1 = (-B + sqrt(D))/A*A
+        let x2 = (-B - sqrt(D))/A*A
+        print("Корни уравнения: \(x1) и \(x2)")
+    } else if D == 0{
+        let x = (-B)/A*A
+        print("Корень уравнения: \(x)")
+    }else if D < 0{
+        print("Корней нет.")
+    }
+}
+
+equation()
+print("Finita")
